3f1d462f1e770f138ca6c2adbd113f5998e13d92
Xarch <rakhaarkhana98@gmail.com>
2025-12-10 13:05:14 +0700

    debug middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,20 +4,17 @@ const User = require("../models/user.model");
    PROTECT MIDDLEWARE (SESSION BASED)
 ========================= */
 const protect = async (req, res, next) => {
-  app.use((req, res, next) => {
-    console.log("COOKIE DITERIMA DARI FRONTEND:", req.headers.cookie);
-    next();
-  });
-
   try {
-    // 1. Cek apakah ada session
+    console.log("SESSION DI PROTECT:", req.session);
+
+    // 1. Pastikan user sudah login (ada session)
     if (!req.session || !req.session.userId) {
       return res.status(401).json({
         message: "Unauthorized. Silakan login.",
       });
     }
 
-    // 2. Cari user dari database
+    // 2. Ambil user dari database
     const user = await User.findById(req.session.userId).select(
       "-passwordHash"
     );
@@ -28,15 +25,16 @@ const protect = async (req, res, next) => {
       });
     }
 
-    // 3. Simpan user ke request agar bisa dipakai di controller
+    // 3. Simpan ke req
     req.user = user;
 
-    // 4. Lanjut ke controller
+    // 4. Lanjut
     next();
   } catch (error) {
     console.error("AUTH MIDDLEWARE ERROR:", error);
     res.status(500).json({
       message: "Terjadi kesalahan di middleware auth.",
+      error: error.message,
     });
   }
 };
