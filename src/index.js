fe3c13c23af0b0741218cff97d072e0f92da0dbc
Xarch <rakhaarkhana98@gmail.com>
2025-12-10 12:10:04 +0700

    debug

--- src/index.js
+++ src/index.js
@@ -49,8 +49,6 @@ app.use(
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      domain: "rumpi-one.vercel.app", // ⭐ FIX PALING PENTING
-      path: "/",
       maxAge: 1000 * 60 * 60 * 24 * 7,
     },
   })
