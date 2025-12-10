75bbe29304b4eabdf4f470cf48484f2198c4b2cf
Xarch <rakhaarkhana98@gmail.com>
2025-12-10 11:04:29 +0700

    change endpoint

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 // 3. Pasang Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://rumpi-one.vercel.app/",
     credentials: true,
   },
 });
