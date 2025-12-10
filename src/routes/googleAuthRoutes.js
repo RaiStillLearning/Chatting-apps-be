9eb8b2241d4a2ac26df38507f56242bda5209989
Xarch <rakhaarkhana98@gmail.com>
2025-12-10 11:27:22 +0700

    tes ombak

--- src/routes/googleAuthRoutes.js
+++ src/routes/googleAuthRoutes.js
@@ -7,5 +7,8 @@ router.get("/google", googleAuthController.googleAuth);
 
 // STEP 2: Callback Google
 router.get("/google/callback", googleAuthController.googleCallback);
+router.get("/google/callback", (req, res) => {
+  console.log("GOOGLE CALLBACK HIT:", req.query);
+});
 
 module.exports = router;
